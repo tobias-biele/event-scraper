b3881ccc4251a85edf077201d95a709482faadfa
Tobias Biele <biele@adelphi.de>
2023-11-01 08:07:16 +0100

    Create data directory if it does not exist

--- excel.py
+++ excel.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 
 def create_sheet(data):
@@ -6,6 +7,9 @@ def create_sheet(data):
         "Veranstaltende Institution/Organisation", "Zielgruppe", "Link zur VA", "Eintragsdatum", "Detailseite Text"
     ]
     df = pd.DataFrame(data, columns=columns)
+    # Create data directory if it doesn't exist
+    if not os.path.exists("data"):
+        os.makedirs("data")
     excel_writer = pd.ExcelWriter("data/events.xlsx", engine="xlsxwriter")
     df.to_excel(excel_writer, sheet_name="Veranstaltungen", index=False)
     excel_writer._save()
