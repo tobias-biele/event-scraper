3037b52a7cd953dc2b38203bce94fb336a8ceaf3
Tobias Biele <biele@adelphi.de>
2023-11-01 08:23:53 +0100

    Add missing actors

--- actors.py
+++ actors.py
@@ -1,4 +1,16 @@
 actors = {
+    "bbsr": {
+        "parser_module": "parser.bbsr_html",
+        "url": "https://www.bbsr.bund.de/BBSR/DE/service/veranstaltungen/_node.html",
+    },
+    "bmbf": {
+        "parser_module": "parser.bmbf_html",
+        "url": "https://www.bildung-forschung.digital/digitalezukunft/de/mediathek/veranstaltungen/_functions/teaserlist_e132d2ec54",
+    },
+    "bmel": {
+        "parser_module": "parser.bmel_html",
+        "url": "https://www.bmel.de/SiteGlobals/Forms/Suche/DE/Terminsuche/Terminsuche_Formular.html",
+    },
     "bmuv": {
         "parser_module": "parser.bmuv_rss",
         "url": "https://www.bmuv.de/events.rss",
