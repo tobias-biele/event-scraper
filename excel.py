87bcb5ca0dc7b6a1fc162c645c6c29370d6e7237
Tobias Biele <biele@adelphi.de>
2023-11-02 10:26:14 +0100

    Add keywords filter

--- excel.py
+++ excel.py
@@ -1,7 +1,8 @@
 import os
 import pandas as pd
+from event import Event
 
-def create_sheet(data):
+def create_sheet(data, filename="data/events.xlsx"):
     columns = [
         "Von", "Bis", "Uhrzeit", "Titel der Veranstaltung", "Thema",
         "Veranstaltende Institution/Organisation", "Zielgruppe", "Link zur VA", "Eintragsdatum", "Detailseite Text"
@@ -10,6 +11,44 @@ def create_sheet(data):
     # Create data directory if it doesn't exist
     if not os.path.exists("data"):
         os.makedirs("data")
-    excel_writer = pd.ExcelWriter("data/events.xlsx", engine="xlsxwriter")
+    excel_writer = pd.ExcelWriter(filename, engine="xlsxwriter")
     df.to_excel(excel_writer, sheet_name="Veranstaltungen", index=False)
     excel_writer._save()
+
+def read_sheet(filename):
+    try:
+        df = pd.read_excel(filename, sheet_name="Veranstaltungen")
+        event_list = []
+
+        for _, row in df.iterrows():
+            # Extract the data from the DataFrame
+            start = row["Von"]
+            end = row["Bis"]
+            timeframe = row["Uhrzeit"]
+            title = row["Titel der Veranstaltung"]
+            topic = row["Thema"]
+            actor = row["Veranstaltende Institution/Organisation"]
+            target_group = row["Zielgruppe"]
+            link = row["Link zur VA"]
+            added = row["Eintragsdatum"]
+            description = row["Detailseite Text"]
+
+            # Create an Event object and add it to the list
+            event = Event(
+                title=title,
+                start=start,
+                end=end,
+                timeframe=timeframe,
+                topic=topic,
+                actor=actor,
+                target_group=target_group,
+                link=link,
+                added=added,
+                description=description
+            )
+            event_list.append(event)
+
+        return event_list
+    except Exception as e:
+        print(f"Error reading Excel file: {e}")
+        return []
---
+++ filter.py
@@ -0,0 +1,34 @@
+from excel import read_sheet, create_sheet
+
+keywords = [
+        "Hitze", 
+        "Starkregen", 
+        "Hochwasser", 
+        "Trockenheit", 
+        "Dürre", 
+        "Naturbasiert", 
+        "Biodiversität", 
+        "Stadtgrün", 
+        "Bürgerkommunikation", 
+        "Stadtplanung", 
+        "Bauleitplanung", 
+        "Gesundheit", 
+        "Eigenvorsorge", 
+        "Verbraucherschutz", 
+        "Akteurskommunikation", 
+        "Verkehrsplanung",
+        "Straßengestaltung",
+    ]
+
+def filter_by_keywords():
+    events = read_sheet("data/events.xlsx")
+    included = []
+    excluded = []
+    for event in events:
+        for keyword in keywords:
+            if keyword.lower() in event.title.lower() or keyword.lower() in str(event.description).lower():
+                included.append(event.to_xlsx_row())
+                break
+        excluded.append(event.to_xlsx_row())
+    create_sheet(included, "data/events_filtered.xlsx")
+    create_sheet(excluded, "data/events_filtered_excluded.xlsx")
