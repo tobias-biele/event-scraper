61db68b0c1ed28f37542008d8f677f64598896e8
Tobias Biele <biele@adelphi.de>
2023-11-08 08:45:08 +0100

    Add location and end date to BMUV parser

--- parser/bmuv_html.py
+++ parser/bmuv_html.py
@@ -10,8 +10,12 @@ def parse_details_page(url):
     title_headline = soup.find_all('h1')[0]
     subtitle = title_headline.find_next_sibling('span')
     date_matches = get_date_matches(subtitle.text)
+    start = ""
+    end = ""
     if len(date_matches) > 0:
-        date = date_matches[0]
+        start = date_matches[0]
+    if len(date_matches) > 1:
+        end = date_matches[1]
     # Find timeframe
     time_label_divs = soup.find_all('div', string=lambda text: text and 'zeit' in text.lower())
     time_div = None
@@ -19,7 +23,16 @@ def parse_details_page(url):
         time_label_div = time_label_divs[0]
         time_div = time_label_div.find_next_sibling('div')
 
+    # Find location
+    location_label_divs = soup.find_all('div', string=lambda text: text and 'veranstaltungsort' in text.lower())
+    location_div = None
+    if len(location_label_divs) > 0:
+        location_label_div = location_label_divs[0]
+        location_div = location_label_div.find_next_sibling('div')
+
     return {
-        "date": date,
-        "timeframe": time_div.text.strip() if time_div else ""
+        "start": start,
+        "end": end,
+        "timeframe": time_div.text.strip() if time_div else "",
+        "location": location_div.text.strip() if location_div else "",
     }
--- parser/bmuv_rss.py
+++ parser/bmuv_rss.py
@@ -10,22 +10,30 @@ def parse(feed_url, options):
     today = today_date()
     for entry in parsed_data:
         start = ""
+        end = ""
         timeframe = ""
+        location = ""
         if options["parse_details_pages"]:
             # Parse event details page to get date and timeframe
             try:
                 details = parse_details_page(entry.link)
-                if details["date"]:
-                    start = details["date"]
+                if details["start"]:
+                    start = details["start"]
+                if details["end"]:
+                    end = details["end"]
                 if details["timeframe"]:
                     timeframe = details["timeframe"]
+                if details["location"]:
+                    location = details["location"]
             except Exception as e:
                 print(f"Error parsing details page for {entry.link}: {e}")
 
         event = Event(
             title=entry.title,
             actor="Bundesministerium für Umwelt, Naturschutz und nukleare Sicherheit",
+            location=location,
             start=start,
+            end=end,
             timeframe=timeframe,
             link=entry.link,
             added=today,
